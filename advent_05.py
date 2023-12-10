439babc72d262f847276ccecb3072884e5df3a97
Sam Gehman <sam.gehman@gmail.com>
2023-12-09 19:01:54 -0500

    Day 5 part 1

---
+++ advent_05.py
@@ -0,0 +1,77 @@
+import math
+import re
+from bisect import bisect_left, bisect_right
+
+INPUT_FILE = "inputs/05-input.txt"
+
+
+# From: https://docs.python.org/3.11/library/bisect.html#searching-sorted-lists
+def find_range(a: list[tuple[int]], x, key):
+    """Find leftmost item greater than or equal to x"""
+    i = bisect_left(a, x, key=key)
+    if i != len(a):
+        return a[i]
+    raise ValueError
+
+
+def part1(input_file: str) -> int:
+    with open(input_file) as f:
+        first_line = f.readline()
+        assert first_line.startswith("seeds:")
+        seeds = [int(seed) for seed in first_line.split(":")[1].split()]
+        print("Seeds:", seeds)
+
+        # Read until EOF, building mappings
+        mappings = []
+        while line := f.readline():
+            line = line.rstrip()
+            if not line:
+                continue
+
+            assert "map" in line
+            print(f"Processing mapping for {line}")
+
+            # Process mapping
+            mapping = []
+            while line := f.readline().rstrip():
+                # Note that we swap the src and dest to make it easier to understand
+                dest, src, length = [int(num) for num in line.split()]
+                mapping.append((src, dest, length))
+
+            # Sort mapping by src
+            mapping.sort(key=lambda x: x[0])
+            print(mapping)
+
+            mappings.append(mapping)
+
+        print("Mappings:", mappings)
+
+    min_location = math.inf
+    for seed in seeds:
+        path = [seed]
+        src = seed
+        for mapping in mappings:
+            # TODO: Bisect to the correct mapping (associated to src) and update src to dst
+            # TODO: use the destination as the next source
+
+            i = bisect_right(mapping, src, key=lambda x: x[0])
+            if not i:
+                path.append(src)
+                continue
+
+            src_start, dest_start, length = mapping[i - 1]
+            if src >= src_start + length:
+                path.append(src)
+                continue
+
+            offset = src - src_start
+            src = dest_start + offset
+            path.append(src)
+
+        min_location = min(src, min_location)
+        print(f"Path from seed {seed} to location {src}:", path)
+
+    return min_location
+
+
+print("Part 1:", part1(INPUT_FILE))
