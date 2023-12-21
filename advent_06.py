b719f12344b90aa735617cf5841fa031ef74b5c6
Sam Gehman <sam.gehman@gmail.com>
2023-12-20 21:39:37 -0800

    Day 6 part 2 (bruteforce)

--- advent_06.py
+++ advent_06.py
@@ -1,28 +1,50 @@
+import re
+
 INPUT_FILE = "inputs/06-input.txt"
 
 
-def part1(input_file: str) -> int:
-    def read_ints(f) -> list[int]:
-        return [int(x) for x in f.readline().split()[1:]]
+def calculate_num_wins_bruteforce(time: int, distance: int) -> int:
+    # Bruteforce
+    num_wins = 0
+    for t in range(time):
+        time_remaining = time - t
+        t_distance = t * time_remaining
+        if t_distance > distance:
+            num_wins += 1
 
+    return num_wins
+
+
+def part1(input_file: str) -> int:
     with open(input_file) as f:
-        times = read_ints(f)
-        distances = read_ints(f)
+
+        def read_ints() -> list[int]:
+            return [int(x) for x in f.readline().split()[1:]]
+
+        times = read_ints()
+        distances = read_ints()
         assert len(times) == len(distances)
 
     total_wins = 1
     for time, distance in zip(times, distances):
-        num_wins = 0
-        for t in range(time):
-            time_remaining = time - t
-            t_distance = t * time_remaining
-            # TODO: ties?
-            if t_distance > distance:
-                num_wins += 1
-
-        total_wins *= num_wins
+        total_wins *= calculate_num_wins_bruteforce(time, distance)
 
     return total_wins
 
 
+def part2(input_file: str) -> int:
+    with open(input_file) as f:
+
+        def read_int() -> int:
+            # Ignore "kerning" on the line and join all ints
+            return int("".join(re.findall("[0-9]+", f.readline())))
+
+        time = read_int()
+        distance = read_int()
+
+    # Bruteforce
+    return calculate_num_wins_bruteforce(time, distance)
+
+
 print("Part 1:", part1(INPUT_FILE))
+print("Part 2:", part2(INPUT_FILE))
